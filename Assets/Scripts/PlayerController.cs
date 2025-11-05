55d9859438992aed0e0d072fd5c3bc7d5f8d52c2
OscarLoZa05 <oscarlzafra@gmail.com>
2025-11-05 13:33:08 +0100

    Kayn

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,25 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-[SerializeField]
+    [Header("Movimiento")]
+    public float _playerSpeed = 6;
+    public float _jumpForce = 6;
+
+    [Header("Inputs")]
+    public Vector2 _moveAction;
+    InputAction _moveInput;
+    
+
+    void Awake()
+    {
+        _moveInput = InputSystem.actions["Move"];
+    }
+
     void Update()
     {
-        
+        _moveAction = _moveInput.ReadValue<Vector2>();
     }
 
 }
